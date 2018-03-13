b896295712e5a7053eac45416f74646b8fc1dc69
Kris <kris.bierma@gmail.com>
2018-03-13 11:32:29 -0500

    push changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,54 +13,13 @@ firebase.initializeApp(config);
 
 //var to hold database info
 var database = firebase.database();
-var trainId; //unique number for each train
-var trainNum; //current number of total trains
 
 //reset HTML form to empty
 $("#form")[0].reset();
 
-//increases current train number by 1 when page is loaded
-updateTrainNum();
-
-//function to increase trainNum in db and make trainId=trainNum
-function updateTrainNum(){
-    var t;
-    var deferred=$.Deferred();
-    database.ref().once("value", function(trainIdSnap){
-        console.log(trainIdSnap.val().trainNum);
-        var t=trainIdSnap.val().trainNum;
-        t++;
-        console.log(t);
-        database.ref().update({
-            trainNum:t
-        });
-        trainId=t;
-        console.log(trainId);
-        console.log(trainId);
-        // rendTableNow=true;
-        // renderTable();
-        // return trainId;
-    });
-    deferred.resolve("");
-    return deferred.promise();
-
-};
-
-
-var currentTime;
-function getTime(){
-    //convert first train time to minutes
-    var rightNow=new Date();
-    var hours = rightNow.getHours();
-    var minutes = rightNow.getMinutes();
-    currentTime= hours * 60 + minutes; //converts current time to minutes
-    console.log("currentTime:"+currentTime); //correct
-};
-
 //on click of submit function of all below
 $("#submit").on("click", function(event){
     event.preventDefault();
-    getTime();
 
     //if not all included, alert
         if($("#train-name").val() ==""){
@@ -87,119 +46,99 @@ $("#submit").on("click", function(event){
         var frequency=$("#train-frequency").val().trim();
         console.log(trainName, dest, firstTime, frequency);
 
-    //convert firstTime to minutes
-        var f = firstTime.split(":");
-        firstTime = (+f[0]) * 60 + (+f[1]);
-        console.log("firstTime in min:"+firstTime);//correct
-
-    //calculate next arrival and minutes away
-        var time = parseInt(firstTime) + parseInt(frequency); //temporary var
-        console.log("temp time (firstTime+freq: "+time);//correct
-        var nextArrival;
-        var arrivalKnown = false;
-
-    //does it work if starting time if beyond current time?
-    while (!arrivalKnown){
-        if (time > currentTime){
-            nextArrival = time;
-            arrivalKnown=true;
-            console.log("nextArr: "+nextArrival);
-        }
-        else if (time === currentTime){
-            nextArrival = time;
-            console.log("nextArr: "+nextArrival);
-            arrivalKnown=true;
-        }
-        else {
-            time = parseInt(time) + parseInt(frequency);
-            console.log("nextArr is time: "+time);
-        };
-        var minAway = parseInt(nextArrival) - parseInt(currentTime);
-        console.log("minAway: "+minAway);
-    };
-
     // if (snapshot.child("name").exists() && snapshot.child("otherName").exists()){
     //     set them;
     // }
 
-    //create new tr and insert data into html
-    var newTableRow = $("<tr id="+trainId+">");
-    var newDataName = $("<td id='newDataName"+trainId+"'>");
-    var newDataDest = $("<td id='newDataDest"+trainId+"'>");
-    var newDataFrequency = $("<td id='newDataFrequency"+trainId+"'>");
-    var newDataNextArrival=$("<td id='newDataNextArrival"+trainId+"'>");
-    var newDataMinutesAway=$("<td id='newDataMinutesAway"+trainId+"'>");
+    //conditional to keep out blank info
+    if(trainName!="" && dest!="" && firstTime!="" && frequency!=""){
+    //store info for each train in new database object:
+        database.ref().push({
+            name:capUpper(trainName),
+            destination:capUpper(dest),
+            firstTime:firstTime,
+            frequency:frequency,
+        });
+    };
 
-    $("tbody").append(newTableRow);
-    $(newTableRow).append(newDataName, newDataDest, newDataFrequency, newDataNextArrival, newDataMinutesAway);
-
-    //store info for each train in database object:
-    database.ref("trains/train-"+trainId).set({
-        trainId:trainId,
-        name:capUpper(trainName),
-        destination:capUpper(dest),
-        firstTime:firstTime,
-        frequency:frequency,
-        nextArrivalTime:nextArrival,
-        minutesAway:minAway,
-    });
-
-    // populate html with db info
-    var r = "train-"+trainId;
-    database.ref("trains/"+r).once("value", function(snap){
-        console.log(snap.val());
-        console.log(snap.val().name);
-        $("#newDataName"+trainId).text(snap.val().name);
-        $("#newDataDest"+trainId).text(snap.val().destination);
-        $("#newDataFrequency"+trainId).text(snap.val().frequency);
-        $("#newDataNextArrival"+trainId).text(convertMinutes(snap.val().nextArrivalTime));
-        $("#newDataMinutesAway"+trainId).text(snap.val().minutesAway);
-    });
-
-    //increase trainNum in db by 1
-    updateTrainNum();
     $("#form")[0].reset();
-});
+}); //end on.click submit
 
-//renders the table with everything in db only when page loads
-$.when(updateTrainNum()).then(
-function renderTable(){
-    //how to reference parents/root???
-    var query=database.ref("trains").orderByChild("trainId");
-    console.log(query);
-    query.once("value").then(function(snap){
-            console.log(snap);
-            snap.forEach(function(trainSnap){
-                var key=trainSnap.key;
-                var ts=trainSnap.val();
-                var trainId=ts.trainId;
+database.ref().on("child_added", function(snapshot){
+    if (snapshot.val()){
+        var data = snapshot.val();
 
-                var newTableRow = $("<tr id="+trainId+">");
-                console.log("new tr");
-                var newDataName = $("<td id='newDataName"+trainId+"'>");
-                var newDataDest = $("<td id='newDataDest"+trainId+"'>");
-                var newDataFrequency = $("<td id='newDataFrequency"+trainId+"'>");
-                var newDataNextArrival=$("<td id='newDataNextArrival"+trainId+"'>");
-                var newDataMinutesAway=$("<td id='newDataMinutesAway"+trainId+"'>");
+        //calculate next arrival
+        getTime(); //gets current time
 
-                $("tbody").append(newTableRow);
-                $(newTableRow).append(newDataName, newDataDest, newDataFrequency, newDataNextArrival, newDataMinutesAway);
+        //convert firstTime (from user input) to minutes
+        var f = data.firstTime.split(":");
+        f = (+f[0]) * 60 + (+f[1]);
+        console.log("firstTime in min:"+f);
 
-                var name=ts.name;
-                $("#newDataName"+trainId).text(name);
-                $("#newDataDest"+trainId).text(ts.destination);
-                $("#newDataFrequency"+trainId).text(ts.frequency);
-                $("#newDataNextArrival"+trainId).text(convertMinutes(ts.nextArrivalTime));
-                $("#newDataMinutesAway"+trainId).text(ts.minutesAway);
-            })
-        })
-    })
+        //temporary var
+        var time = parseInt(f) + parseInt(data.frequency); 
+        console.log("temp time (firstTime+freq: "+time);
+        var nextArrival;
+        var arrivalKnown = false;
+
+        //does it work if starting time if beyond current time?
+        while (!arrivalKnown){
+            if (time > currentTime){
+                nextArrival = time;
+                arrivalKnown=true;
+                console.log("nextArr: "+nextArrival);
+            }
+            else if (time === currentTime){
+                nextArrival = time;
+                console.log("nextArr: "+nextArrival);
+                arrivalKnown=true;
+            }
+            else {
+                time = parseInt(time) + parseInt(data.frequency);
+                console.log("nextArr is time: "+time);
+            };
+            
+            //calculate minutes away
+            var minAway = parseInt(nextArrival) - parseInt(currentTime);
+            console.log("minAway: "+minAway);
+        };
+
+
+
+        //create new tr and insert data into html
+        var newTableRow = $("<tr>");
+        var newDataName = $("<td>").text(data.name);
+        var newDataDest = $("<td>").text(data.destination);
+        var newDataFrequency = $("<td>").text(data.frequency);
+        var newDataNextArrival=$("<td>").text(convertMinutes(nextArrival));
+        var newDataMinutesAway=$("<td>").text(minAway);
+
+
+        newTableRow.append(newDataName, newDataDest, newDataFrequency, newDataNextArrival, newDataMinutesAway);
+        $("tbody").append(newTableRow);
+    };//end if statement
+});//end "child_added"
+
+//function to get current time
+var currentTime;
+function getTime(){
+    //convert first train time to minutes
+    var rightNow=new Date();
+    var hours = rightNow.getHours();
+    var minutes = rightNow.getMinutes();
+    currentTime= hours * 60 + minutes; //converts current time to minutes
+    console.log("currentTime:"+currentTime); //correct
+};
 
 //converts minutes into HH:MM
 function convertMinutes(event){
     console.log("event should be temp time:" +event);//correct
     //event = time in minutes
     var m = event%60;
+    // if (m===0){
+    //     m=00;
+    // };
     var h = Math.floor(event/60);
     var convertedTime = h + ":" + m;
     console.log("Should be temp time in HH:MM " +convertedTime);//correct
@@ -211,9 +150,9 @@ function capUpper(name){
     return name.charAt(0).toUpperCase() + name.slice(1);
 };
 
+
 });//end doc.ready
 
 //continually update 
 //users from different machines able to view
 //add extra 0 to end of next arrival time
-//restart trainId on db to 0
