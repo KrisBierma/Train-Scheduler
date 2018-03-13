d5bebb62101da3ba5a81a25fd7c19c0c4f6405b6
Kris <kris.bierma@gmail.com>
2018-03-13 16:08:10 -0500

    added to-do list at bottom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -204,4 +204,11 @@ $(document).on("click", function(e){
 });
 
 });//end doc.ready
-//users from different machines able to view
+
+// delete row on html
+
+// Users from many different machines must be able to view same train times.
+
+// As a final challenge, make it so that only users who log into the site with their Google or GitHub accounts can use your site. You'll need to read up on Firebase authentication for this bonus exercise.
+
+// Try adding update button for each train. Let the user edit the row's elements-- allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
