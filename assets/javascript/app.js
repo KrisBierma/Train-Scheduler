b903ef6bf57b0cfa90db9f0faf949e01701e23d9
Kris <kris.bierma@gmail.com>
2018-03-13 16:35:44 -0500

    deleteBtn cauing high usage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -194,14 +194,13 @@ function capUpper(name){
     return name.charAt(0).toUpperCase() + name.slice(1);
 };
 
-$(document).on("click", function(e){
+$(document).on("click", "#deleteBtn", function(e){
     console.log(e);
     var rowId=e.target.parentElement.parentElement.id;
     console.log(rowId);
     //delete row not working
     // $(rowId).deleteRow;
     database.ref().child(rowId).remove();
-
 });
 
 });//end doc.ready
